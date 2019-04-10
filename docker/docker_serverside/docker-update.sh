c2f070d96f359bd6db089a7b472dae9d4f104baf
Pierre-Henri <pierreh37@gmail.com>
2019-04-10 14:54:52 +0200

    Docker clean up

--- docker/docker_serverside/docker-update.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo docker-compose down
-sudo docker-compose up -d
--- docker/publish.bat
+++
@@ -1,3 +0,0 @@
-docker login
-docker tag funnychain:latest registry.admin.rphstudio.net/funnychain:latest
-docker push registry.admin.rphstudio.net/funnychain:latest
