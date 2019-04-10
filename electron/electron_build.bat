444d933c1e527c3d7f2eb63eb3be33803869a4dc
Pierre-Henri <pierreh37@gmail.com>
2019-04-10 14:13:55 +0200

    Add electron support

---
+++ electron/electron_build.bat
@@ -0,0 +1,18 @@
+rem install node js
+
+rem clean up previous build
+rmdir /s/q .\out\
+rmdir /s/q .\src\
+mkdir src
+xcopy /s .\build\index.js .\src\
+
+rem RUN NPM
+cd ..
+call npm install
+call npm run build
+xcopy /s .\build .\electron\src\
+cd electron
+
+rem RUN electron
+yarn install
+yarn make
